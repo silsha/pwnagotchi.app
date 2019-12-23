142ddd4bb8e8e59049e08b44834134f8a9c3b325
Silsha Fux <hallo@silsha.me>
2019-12-23 11:50:48 +0100

    Initial Commit

---
+++ ios-pwnagotchi WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ios-pwnagotchi WatchKit Extension
+//
+//  Created by Silsha Fux on 23.12.2019.
+//  Copyright © 2019 fnordcordia. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios-pwnagotchi WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  ios-pwnagotchi WatchKit Extension
+//
+//  Created by Silsha Fux on 23.12.2019.
+//  Copyright © 2019 fnordcordia. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
